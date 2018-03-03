7d05e3c61bf07bf2e6d5db2ea20fa6fcbf7c85f2
csmx1234@163.com <csm@csms-Mac-mini.local>
2018-03-03 23:30:01 +0800

    added gulp sass browser-sync nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,37 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var sass        = require('gulp-sass');
+var nodemon     = require('gulp-nodemon');
 var browserSync = require('browser-sync').create();
 
 gulp.task('scss', function () {
 	return gulp.src('./public/scss/*.scss')
 	.pipe(sass().on('error', sass.logError))
-	.pipe(gulp.dest('./public/stylesheets'));
+	.pipe(gulp.dest('./public/stylesheets'))
+	.pipe(reload({ stream: true }));
 });
 
-gulp.task('watch', function () {
-	browserSync.init({
-		server: {
-			baseDir: "./"
+gulp.task('nodemon', function (cb) {
+	var started = false;
+	return nodemon({
+		script: './bin/www'
+	}).on('start', function () {
+		if (!started) {
+			cb();
+			started = true;
 		}
+	});
+});
+
+gulp.task('watch', ['nodemon'], function () {
+	browserSync.init({
+		browser: "google chrome",
+		proxy: "localhost:3000",
+		port: 3001,
+		notify: false
 	});
 	gulp.watch('./public/scss/*.scss', ['scss']);
 	gulp.watch("./views/*.pug").on("change", browserSync.reload);
+	gulp.watch("./public/**/*.*").on("change", browserSync.reload);
 });
